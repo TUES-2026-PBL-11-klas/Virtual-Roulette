e5c0421a3a53699ed249eabbd4d4ed385b772efe
Yoshitd <mihaylovyoan.ym@gmail.com>
2026-02-22 17:49:07 +0200

    finally a good str

--- frontend/virrule/src/App.jsx
+++ frontend/virrule/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '../styles/index.css'
+import './styles/index.css'
 import Home from './pages/Home';
 import Login from './pages/Login';
 
